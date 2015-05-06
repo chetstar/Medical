135d4e4bcf4ca542870d0e7e23cbbdc8ba4cd9d0
Gregory Arenius <gregory@arenius.com>
2015-05-06 09:04:50 -0700

    Fixed df.info bug

--- source/medical_main.py
+++ source/medical_main.py
@@ -7,22 +7,21 @@ import config
 
 start_time = datetime.datetime.now()
 print('Started at: {}'.format(start_time))
-print('Data frame info: ',df.info)
 
 #Create a pandas dataframe (df) from medical_file.
 df = medical_functions.load_medical_data(config.medical_file)
 print('Medi-Cal file finished loading at: {}'.format(datetime.datetime.now()))
-print('Data frame info: ',df.info)
+print('Data frame info: ',df.info())
 
 #Drop duplicate row, rows with no CINs, and fix mispelt city names.
 df = medical_functions.clean_medical_data(df)
 print('clean_medical_data finished at: {}'.format(datetime.datetime.now()))
-print('Data frame info: ',df.info)
+print('Data frame info: ',df.info())
 
 #Add bday, calendar, HCplanText, ethnicity, language, and region columns.
 df = medical_functions.add_supplementary_columns(df)
 print('add_supplementary_columns finished at: {}'.format(datetime.datetime.now()))
-print('Data frame info: ',df.info)
+print('Data frame info: ',df.info())
 
 #Save off medsCurrentUncut.sav file.
 medical_functions.create_meds_current_uncut(df)
@@ -32,15 +31,16 @@ print('Uncut.sav finished at: {}'.format(datetime.datetime.now()))
 #df = medical_functions.wide_to_long_by_month(df)
 df = medical_functions.wide_to_long(df)
 print('wide_to_long finished at: {}'.format(datetime.datetime.now()))
-print('Data frame info: ',df.info)
+print('Data frame info: ',df.info())
 
 df = medical_functions.cal(df)
 print('cal finished at: {}'.format(datetime.datetime.now()))
-print('Data frame info: ',df.info)
+print('Data frame info: ',df.info())
 
 #Create a single column, MCelig, with a boolean eligibility status.
 df = df.apply(medical_functions.create_mcelig, axis = 1)
 print('MCelig created at: {}'.format(datetime.datetime.now()))
+print('Data frame info: ',df.info())
 
 #Drop months where there is no eligibility.
 df = medical_functions.drop_ineligible_months(df)
