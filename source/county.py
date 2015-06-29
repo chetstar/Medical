4825770d18cb1aa6797e84b0f83230454e4545a7
Gregory Arenius <gregory@arenius.com>
2015-06-29 15:24:56 -0700

    Create program to make countycase.sav files.

---
+++ source/county.py
@@ -0,0 +1,36 @@
+import json
+import datetime
+
+from savReaderWriter import SavWriter #For saving SPSS .sav files.
+import pandas as pd
+
+import config
+
+if __name__ == "__main__":
+
+    program_start_time = datetime.datetime.now()
+    
+    #column_names and column_specifications are used by pandas.read_fwf to read Medi-Cal file.
+    with open(config.county_load_info) as fp:
+        column_names, column_specifications = zip(*json.load(fp))
+
+    #All columns should be brought in as strings.
+    converters = {name:str for name in column_names}
+
+    df = pd.read_fwf(config.medical_file,
+                     colspecs = column_specifications,
+                     header = None,
+                     names = column_names, 
+                     converters = converters )
+
+    with open(config.county_save_info) as fp:
+        save_info = json.load(fp)
+
+    with SavWriter(config.county_file, 
+                   save_info['column_names'], 
+                   save_info['types'], 
+                   measureLevels = save_info['measure_levels'],
+                   alignments = save_info['alignments'],
+                   columnWidths = save_info['column_widths']) as writer:
+
+        writer.writerows(df[save_info['column_names']].values)
