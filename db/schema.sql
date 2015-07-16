46c82ba33fe37050355b43404c6cf22e134fdc86
Gregory Arenius <gregory@arenius.com>
2015-07-15 20:09:18 -0700

    Rename hic to hic_number.

--- db/schema.sql
+++ db/schema.sql
@@ -31,18 +31,20 @@ CREATE TABLE "hcp_statuses" (
 CREATE TYPE sex_enum AS ENUM ('Male','Female','Intersex','Unknown','Other');
 
 CREATE TABLE "client_attributes" (
+       -- cin = client index number
+       -- hic = health insurance claim
        "id" BIGSERIAL PRIMARY KEY,
        "cin" TEXT NOT NULL,
        "date_of_birth" DATE,
        "meds_id" TEXT,
-       "hic" TEXT,
+       "hic_number" TEXT, 
        "hic_suffix" TEXT,
-       "ethnicity" TEXT, --Constrain
-       "language" TEXT, --Constrain
-       "sex" SEX_ENUM,
+       "race" TEXT, --Make table to constrain to. Store English term, not code.
+       "sex" SEX_ENUM, 
+       "primary_language" TEXT, --Make table to constrain to. Store English term, not code.
        CONSTRAINT client_attributes_CK_cin_length CHECK (char_length(cin) <= 9),
        CONSTRAINT client_attributes_CK_meds_id_length CHECK (char_length(meds_id) <= 9),
-       CONSTRAINT client_attributes_CK_hic_length CHECK (char_length(hic) <= 9),
+       CONSTRAINT client_attributes_CK_hic_number_length CHECK (char_length(hic_number) <= 9),
        CONSTRAINT client_attributes_CK_hic_suffix_length CHECK (char_length(hic_suffix) <= 2),
        CONSTRAINT client_attributes_CK_date CHECK 
        		  (date_of_birth > to_date('1895-01-01','YYYY-MM-DD')),
@@ -132,9 +134,6 @@ CREATE TABLE "client_eligibility_status" (
        CONSTRAINT client_eligibility_status_FK_cin FOREIGN KEY (cin)
        		  REFERENCES client_attributes (cin) ON DELETE RESTRICT
 );
-
-
-
 
 /*
 INSERT INTO client_eligibility_status (cin, "date", cardinal, aidcode) 
